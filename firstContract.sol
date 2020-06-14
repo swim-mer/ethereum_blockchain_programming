9defa51a8d098005d4d34660494a697533bd1a56
swim-mer <kelly.talk.api@gmail.com>
2020-06-13 17:03:16 -0700

    Update firstContract.sol

--- firstContract.sol
+++ firstContract.sol
@@ -1,12 +1,3 @@
-/**
- * First Smart Contract exercise - storage of value.
- * 
- * Contract is collection of code (functions) and data (state)
- * residing at specific address on Ethereum blockchain.
- */
-
-// Current latest version: 0.6.10
-// Written for Solidity v. 0.6.10 or newer, up to 0.7.0
 pragma solidity ^0.6.10;
 
 contract FirstContract {
